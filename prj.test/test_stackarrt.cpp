527db3c417cf9a98bc7187e51dfa2fcf991c9771
Eugene Michkov <michkov.evgeny@gmail.com>
2024-04-19 12:09:33 +0300

    added tests for stackarrt

--- prj.test/test_stackarrt.cpp
+++ prj.test/test_stackarrt.cpp
@@ -70,3 +70,64 @@ TEST_CASE_TEMPLATE("move constructor", T, int, double, char) {
   stack_move.Pop();
   CHECK(stack_move.IsEmpty());
 }
+
+TEST_CASE_TEMPLATE("lifo", T, int, double, char) {
+  StackArrT<T> stack;
+  stack.Push(1);
+  stack.Push(2);
+  stack.Push(3);
+  CHECK(stack.Top() == 3);
+  stack.Pop();
+  CHECK(stack.Top() == 2);
+  stack.Pop();
+  CHECK(stack.Top() == 1);
+  stack.Pop();
+  CHECK(stack.IsEmpty());
+}
+
+TEST_CASE_TEMPLATE("assignment operator", T, int, double, char) {
+  StackArrT<T> stack;
+  stack.Push(1);
+  stack.Push(2);
+  stack.Push(3);
+  StackArrT<T> stack_copy;
+  stack_copy = stack;
+  CHECK(stack_copy.Top() == 3);
+  stack_copy.Pop();
+  CHECK(stack_copy.Top() == 2);
+  stack_copy.Pop();
+  CHECK(stack_copy.Top() == 1);
+  stack_copy.Pop();
+  CHECK(stack_copy.IsEmpty());
+}
+
+TEST_CASE_TEMPLATE("move assignment operator", T, int, double, char) {
+  StackArrT<T> stack;
+  stack.Push(1);
+  stack.Push(2);
+  stack.Push(3);
+  StackArrT<T> stack_move;
+  stack_move = std::move(stack);
+  CHECK(stack_move.Top() == 3);
+  stack_move.Pop();
+  CHECK(stack_move.Top() == 2);
+  stack_move.Pop();
+  CHECK(stack_move.Top() == 1);
+  stack_move.Pop();
+  CHECK(stack_move.IsEmpty());
+}
+
+TEST_CASE_TEMPLATE("copy constructor self assignment", T, int, double, char) {
+  StackArrT<T> stack;
+  stack.Push(1);
+  stack.Push(2);
+  stack.Push(3);
+  stack = stack;
+  CHECK(stack.Top() == 3);
+  stack.Pop();
+  CHECK(stack.Top() == 2);
+  stack.Pop();
+  CHECK(stack.Top() == 1);
+  stack.Pop();
+  CHECK(stack.IsEmpty());
+}
